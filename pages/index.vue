e7251f303f9f9b38d0eb05120ca8e1d2649ba839
DriesBos <info@driesbos.com>
2023-11-02 18:07:27 +0000

    content: images

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,31 @@
 <template>
-  <p>TESTY</p>
+  <div>
+    <ul class="landscape">
+      <li><img src="~assets/img/layer1.png" alt="" /></li>
+      <li><img src="~assets/img/layer2.png" alt="" /></li>
+      <li><img src="~assets/img/layer3.png" alt="" /></li>
+      <li><img src="~assets/img/layer4.png" alt="" /></li>
+      <li><img src="~assets/img/layer5.png" alt="" /></li>
+      <li><img src="~assets/img/layer6.png" alt="" /></li>
+      <li><img src="~assets/img/layer7.png" alt="" /></li>
+      <li><img src="~assets/img/layer8.png" alt="" /></li>
+    </ul>
+  </div>
 </template>
+
+<style scoped lang="sass">
+.landscape
+  border:1px solid red
+  ul
+    width:100vw
+    display: flex
+    flex-direction: column
+    justify-content: flex-start
+    border: 1px solid green
+    li
+      border: 1px solid blue
+      width: 100vw
+      height: 50vh
+      img
+        width: 100vw
+</style>
