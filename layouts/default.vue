15e21b470071f2ad977b6b9aada0e4535256fa9a
DriesBos <info@driesbos.com>
2023-11-10 08:37:13 +0000

    pagescroll: scrolltrigger panels

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,32 +1,97 @@
 <template>
   <main>
     <!-- <TheHeader /> -->
-    <!-- <div class="cursor" ref="cursor" />
-    Mouse position is at: {{ x }}, {{ y }} -->
+    <div class="cursor-outline"></div>
+    <div class="cursor-dot"></div>
     <slot />
   </main>
 </template>
 
 <script setup>
-import { onMounted, onUnmounted } from 'vue';
+import { onMounted } from 'vue';
 import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import { ScrollToPlugin } from 'gsap/ScrollToPlugin';
-import { useMouse } from '../hooks/mouse';
 
-gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+gsap.set('.cursor-dot', { scale: 0.1 });
+gsap.set('.cursor-outline', { scale: 0.5 });
 
-const { x, y } = useMouse();
+let xCTo = gsap.quickTo('.cursor-outline', 'left', {
+  duration: 0.2,
+  ease: 'power3',
+});
+let yCTo = gsap.quickTo('.cursor-outline', 'top', {
+  duration: 0.2,
+  ease: 'power3',
+});
 
-function moveCursor(props) {
-  gsap.to('.cursor', 0.1, {
-    opacity: 1,
-    left: props.clientX,
-    top: props.clientY,
-    ease: 'ease',
-  });
+let xDTo = gsap.quickTo('.cursor-dot', 'left', {
+  duration: 0.6,
+  ease: 'power3',
+});
+let yDTo = gsap.quickTo('.cursor-dot', 'top', {
+  duration: 0.6,
+  ease: 'power3',
+});
+
+let isVisible = false;
+
+function mouseMove(e) {
+  if (!isVisible) {
+    gsap.set('.cursor-outline, .cursor-dot', { opacity: 1 });
+    isVisible = true;
+  }
+
+  const cursorPosition = {
+    left: e.clientX,
+    top: e.clientY,
+  };
+
+  xCTo(cursorPosition.left);
+  yCTo(cursorPosition.top);
+  xDTo(cursorPosition.left);
+  yDTo(cursorPosition.top);
 }
 
-onMounted(() => window.addEventListener('mousemove', moveCursor));
-onUnmounted(() => window.removeEventListener('mousemove', moveCursor));
+let scaleAnim = gsap.timeline({ paused: true });
+
+scaleAnim
+  .to('.cursor-outline', {
+    scale: 1,
+  })
+  .to(
+    '.cursor-dot',
+    {
+      scale: 1,
+      duration: 0.35,
+    },
+    0
+  );
+
+onMounted(() => {
+  document.addEventListener('mousemove', mouseMove);
+});
 </script>
+
+<style scoped lang="sass">
+.cursor-outline, .cursor-dot
+  opacity: 0
+
+.cursor-outline
+  border-radius: 50px
+  position: absolute
+  width: 80px
+  height: 80px
+  border: solid 1px #fff
+  mix-blend-mode: difference
+  pointer-events: none
+
+/* Visible when running fancy.js */
+.cursor-dot
+  mix-blend-mode: difference
+  border-radius: 50px
+  position: absolute
+    width: 80px
+  height: 80px
+  background-color: white
+  border: solid 1px #fff
+  pointer-events: none
+</style>
--- pages/versiontwo.vue
+++ pages/versiontwo.vue
@@ -2,21 +2,63 @@
   <div>
     <Celestials />
     <ul class="landscape">
-      <li class="panel"><img src="~assets/img/layer1.png" alt="" /></li>
-      <li class="panel"><img src="~assets/img/layer2.png" alt="" /></li>
-      <li class="panel"><img src="~assets/img/layer3.png" alt="" /></li>
-      <li class="panel"><img src="~assets/img/layer4.png" alt="" /></li>
-      <li class="panel"><img src="~assets/img/layer5.png" alt="" /></li>
-      <li class="panel"><img src="~assets/img/layer6.png" alt="" /></li>
-      <li class="panel"><img src="~assets/img/layer7.png" alt="" /></li>
-      <li class="panel"><img src="~assets/img/layer8.png" alt="" /></li>
+      <li class="panel">
+        <img src="~assets/img/layer1.png" alt="" />
+      </li>
+      <li class="panel landpanel">
+        <img src="~assets/img/layer2.png" alt="" />
+      </li>
+      <li class="panel landpanel">
+        <img src="~assets/img/layer3.png" alt="" />
+      </li>
+      <li class="panel landpanel">
+        <img src="~assets/img/layer4.png" alt="" />
+      </li>
+      <li class="panel landpanel">
+        <img src="~assets/img/layer5.png" alt="" />
+      </li>
+      <li class="panel landpanel">
+        <img src="~assets/img/layer6.png" alt="" />
+      </li>
+      <li class="panel landpanel">
+        <img src="~assets/img/layer7.png" alt="" />
+      </li>
+      <li class="panel landpanel">
+        <img src="~assets/img/layer8.png" alt="" />
+      </li>
     </ul>
   </div>
 </template>
 
 <script setup>
+import { onMounted, onUnmounted, ref } from 'vue';
+import gsap from 'gsap';
+
 definePageMeta({
   title: 'On Scroll',
+});
+
+const main = ref();
+let ctx;
+
+onMounted(() => {
+  const boxes = document.querySelectorAll('.landpanel');
+  console.log(boxes);
+  boxes.forEach((box) => {
+    gsap.to(box, {
+      y: -50,
+      scrollTrigger: {
+        trigger: box,
+        start: 'top 70%',
+        end: 'top 50%',
+        scrub: true,
+      },
+    });
+  });
+});
+
+onUnmounted(() => {
+  // ctx.revert(); // <- Easy Cleanup!
 });
 </script>
 
@@ -29,6 +71,8 @@ definePageMeta({
   justify-content: flex-start
   li
     width: 100vw
+    &.landpanel
+      margin-top: -25%
     img
       width: 100%
       max-width: 100%
