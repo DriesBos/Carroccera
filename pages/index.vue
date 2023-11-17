3bc57d0a6d3d8c4e96113c48465b2e7ffcec868f
DriesBos <info@driesbos.com>
2023-11-17 09:16:59 +0000

    modal: two column

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
       <li class="panel"><img src="~assets/img/layer1.png" alt="" /></li>
       <li class="panel landscapePanel landpanel">
         <img src="~assets/img/layer2.png" alt="" />
-        <div class="building" @click="openModalSide">
+        <div class="building building-One" @click="openModalSide">
           <img src="~assets/img/sketch1.png" alt="" />
         </div>
       </li>
@@ -14,11 +14,14 @@
       </li>
       <li class="panel landscapePanel landpanel">
         <img src="~assets/img/layer4.png" alt="" />
+        <div class="building building-Two" @click="openModalCenter">
+          <img src="~assets/img/sketch2.png" alt="" />
+        </div>
       </li>
       <li class="panel landscapePanel landpanel">
         <img src="~assets/img/layer5.png" alt="" />
-        <div class="building" @click="openModalCenter">
-          <img src="~assets/img/sketch2.png" alt="" />
+        <div class="building building-Three" @click="openModalCenterTwo">
+          <img src="~assets/img/sketch4.png" alt="" />
         </div>
       </li>
       <li class="panel landscapePanel landpanel">
@@ -141,6 +144,66 @@
       </div>
     </div>
   </Transition>
+  <Transition name="fade">
+    <div
+      v-if="isModalCenterTwoVisible"
+      class="modal modalCenter"
+      :class="{ active: isModalCenterTwoVisible }"
+    >
+      <div class="modalClose cursorInteract icon" @click="closeModalCenterTwo">
+        <img src="~assets/icons/close.png" alt="" />
+      </div>
+      <div class="twoColumn">
+        <div class="twoColumn_Left">
+          <div class="block_Text">
+            <p>
+              Together with a team of ecologists the Collective is developing a
+              proposal for the Carroccera property and the Stanavasso valley at
+              large, in order to preserve and encourage the already present
+              rewilding processes.
+            </p>
+            <p>
+              The 5 hectares site is partially covered by 30 years old
+              spontaneous vegetation and partially by young grassland. Around
+              the site it is possible to spot the signs of a past wineyard and
+              several wells used for agriculture. This abandonment is not
+              necessarily bad but it can be beneficial if treated in a
+              particular way. Additionally there is potential to involve the
+              neighbours and their fallow plots in the scheme of regeneration,
+              achieving a mosaic of natural and agricultural land coexisting in
+              symbiosis.
+            </p>
+            <p>
+              A few active and passive actions which focus on restoring and
+              reinstating a range of natural processes, habitats and missing
+              species, which would strengthen the ecosystem and bring several
+              benefits to the area.
+            </p>
+            <p>
+              The proposal involves site clearance from man made debris and
+              invasive species. Followed by the introduction of self-supporting
+              cattle as grazers to naturally diversify and strengthen the local
+              ecosystem, implemented through the installation of livestock water
+              facility, cattle grids and non-invasive fences. Data is collected,
+              analysed and publicly shared through a network of sensors
+              strategically positioned on the site with the aim of monitoring
+              the biodiversity and inspiring other parties to join us at this
+              endeavour.
+            </p>
+          </div>
+        </div>
+        <div class="twoColumn_Right">
+          <div class="block_Image">
+            <img src="~assets/img/content1.jpg" alt="" />
+          </div>
+
+          <div class="block_Image">
+            <img src="~assets/img/content2.jpg" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </Transition>
 </template>
 
 <style scoped lang="sass">
@@ -159,8 +222,13 @@
     .building
       position: absolute
       top: 0
-      left: 10vw
       cursor: pointer
+      &-One
+        left: random(90) + vw
+      &-Two
+        left: random(90) + vw
+      &-Three
+        left: random(90) + vw
       img
         width: 300px
         height: auto
@@ -184,6 +252,7 @@
 .modal
   overflow: auto
   padding: 5rem
+  padding-bottom: 0
   position: fixed
   background: white
   z-index: +2
@@ -207,9 +276,25 @@
   right: 3rem
   bottom: 3rem
   left: 3rem
+  border-radius: .5rem
 
 .modalContent
   max-width: 50vw
+  padding-bottom: 3rem
+
+.twoColumn
+  display: flex
+  flex-direction: row
+  justify-content: space-between
+  width: 100%
+  height: 100%
+  &_Left
+    flex: 1
+    padding-right: 1rem
+    position: sticky
+  &_Right
+    flex: 1
+    padding-left: 1rem
 
 .block_Image
   width: 100%
@@ -277,5 +362,17 @@ function openModalSide() {
 function closeModalSide() {
   isModalSideVisible.value = false;
   console.log(isModalSideVisible.value);
+}
+
+const isModalCenterTwoVisible = ref(false);
+
+function openModalCenterTwo() {
+  isModalCenterTwoVisible.value = true;
+  console.log(isModalCenterTwoVisible.value);
+}
+
+function closeModalCenterTwo() {
+  isModalCenterTwoVisible.value = false;
+  console.log(isModalCenterTwoVisible.value);
 }
 </script>
