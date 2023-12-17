034a73487edbaa5f66c00445448180cbfc98d17f
DriesBos <info@driesbos.com>
2023-12-17 23:06:26 +0000

    style: responsive slider

--- storyblok/BlokSlider.vue
+++ storyblok/BlokSlider.vue
@@ -110,16 +110,20 @@ export default {
       object-position: center center
 
 .modal-Column_One
-  .square
-    aspect-ratio: auto 1 / 1
-  .landscape
-    aspect-ratio: auto 3 / 2
-  .video
-    aspect-ratio: auto 4 / 3
-  .wide
-    aspect-ratio: auto 16 / 9
-  .full
+  .square, .landscape, .video, .wide
     width: 100%
+  @media screen and ( min-width: $breakpoint-mobile)
+    .square
+      aspect-ratio: auto 1 / 1
+    .landscape
+      aspect-ratio: auto 3 / 2
+    .video
+      aspect-ratio: auto 4 / 3
+    .wide
+      aspect-ratio: auto 16 / 9
+    .full
+      width: 100%
+
 
 .modal-Column_Two
   .blokSlider
