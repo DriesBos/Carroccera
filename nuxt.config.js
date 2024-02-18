d73077dfb9aafada1557849831950a557cbfe95e
DriesBos <info@driesbos.com>
2024-02-18 14:40:48 +0000

    fix: SPA loader test

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,6 +7,7 @@ export default defineNuxtConfig({
     },
   },
   ssr: false,
+  spaLoadingTemplate: './spa-loading-template',
   app: {
     pageTransition: { name: 'page', mode: 'out-in' },
     head: {
