5da9390af42c3a99d5387a3754f63b2811d75103
DriesBos <info@driesbos.com>
2025-12-12 14:38:45 +0900

    cleanup

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -5,25 +5,11 @@ const resolveRelations = ['popular-articles.articles'];
 
 const story = await useAsyncStoryblok(
   slug && slug.length > 0 ? slug.join('/') : 'home',
-  {
-    version: 'draft',
-    resolve_relations: resolveRelations,
-  },
-  {
-    resolveRelations,
-  }
+  { version: 'draft', resolve_relations: resolveRelations },
+  { resolveRelations }
 );
 </script>
 
 <template>
   <StoryblokComponent v-if="story" :blok="story.content" />
 </template>
-
-<!-- For index.vue if needed -->
-<!-- <script setup>
-const story = await useAsyncStoryblok('home', { version: 'draft' });
-</script>
-
-<template>
-  <StoryblokComponent v-if="story" :blok="story.content" />
-</template> -->
