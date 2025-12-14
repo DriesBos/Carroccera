0e42a029fdff1d7c0599913effbfaaefb2b7e068
DriesBos <info@driesbos.com>
2025-12-14 21:11:42 +0900

    fix: internal / external link distinction

--- components/TheNewsList.vue
+++ components/TheNewsList.vue
@@ -5,11 +5,10 @@
       :key="item._uid || item.title || i"
       class="menuItem header-Menu_MediaList"
     >
-      <div v-if="item.link.cached_url" class="dot"></div>
+      <div v-if="item.link?.cached_url" class="dot"></div>
       <NuxtLink
+        v-if="item.link?.cached_url && isInternalLink(item.link.cached_url)"
         class="text"
-        v-if="item.link && item.link.cached_url"
-        :target="item.link.target"
         :to="item.link.cached_url"
         >{{ item.text }}
         <svg
@@ -25,6 +24,26 @@
           />
         </svg>
       </NuxtLink>
+      <a
+        v-else-if="item.link?.cached_url"
+        class="text"
+        :href="getExternalUrl(item.link.cached_url)"
+        :target="item.link.target || '_blank'"
+        rel="noopener noreferrer"
+        >{{ item.text }}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          width="12"
+          height="12"
+          viewBox="0 0 25 25"
+        >
+          <path
+            fill="currentColor"
+            d="M12 1v2H3v19h19v-9h2v11H1V1h11Zm12 7h-2V4.414L9.374 17.04 7.96 15.626 20.585 3H17V1h7v7Z"
+          />
+        </svg>
+      </a>
       <div v-else class="text">{{ item.text }}</div>
       <div v-if="item.date" class="date">{{ formatDate(item.date) }}</div>
     </div>
@@ -62,6 +81,35 @@ try {
   // eslint-disable-next-line no-console
   console.error('Failed to load news for TheNewsList', err);
   newsList.value = [];
+}
+
+/**
+ * Check if a URL is internal (starts with /) or external
+ */
+function isInternalLink(url) {
+  if (!url) return false;
+  return url.startsWith('/');
+}
+
+/**
+ * Get external URL with protocol if missing
+ * Handles URLs like "www.archdaily.com" -> "https://www.archdaily.com"
+ */
+function getExternalUrl(url) {
+  if (!url) return '#';
+
+  // If it already has a protocol, return as-is
+  if (url.startsWith('http://') || url.startsWith('https://')) {
+    return url;
+  }
+
+  // If it starts with //, add https:
+  if (url.startsWith('//')) {
+    return 'https:' + url;
+  }
+
+  // Otherwise, add https://
+  return 'https://' + url;
 }
 
 /**
